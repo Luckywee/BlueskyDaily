4b428ccc49e2ffc8a1b79083a3e4318edf052fbc
Luckywee <83698843+Luckywee@users.noreply.github.com>
2024-10-09 18:09:26 +0200

    Create dailyConfig.py

---
+++ dailyConfig.py
@@ -0,0 +1,10 @@
+ACCOUNTS = {
+    "example": {
+        "BLUESKY_HANDLE": "example.bsky.social",
+        "BLUESKY_PASSWORD": "examplePswd",
+        "FILE_NAME": "example",
+        "HASHTAGS": ["example", "example2", "art"]
+    },
+}
+EMAIL_ADDRESS = "an-email@gmail.com"
+EMAIL_PASSWORD = "an-email-password"
